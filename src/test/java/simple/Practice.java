2795606d36ade1e47e0f0f20cee19dd10e72d44e
sncsrivalli <109685225+sncsrivalli@users.noreply.github.com>
2023-01-07 16:09:12 +0530

    Update Practice.java

--- src/test/java/simple/Practice.java
+++ src/test/java/simple/Practice.java
@@ -1,7 +1,7 @@
 package simple;
 //changes made in branch1
 public class Practice {
-	
+	//changes made in master
 	public static void main(String[] args) {
 		System.out.println("Practice method");
 	}
