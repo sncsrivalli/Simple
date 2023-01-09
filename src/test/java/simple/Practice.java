e8a5b6ade9d7c8905915f6932442599c77b7637d
QPS-Basavanagudi <QPS-Basavanagudi@DESKTOP-F52EDBA>
2023-01-09 15:19:41 +0530

    22

--- src/test/java/simple/Practice.java
+++ src/test/java/simple/Practice.java
@@ -2,7 +2,7 @@ package simple;
 //123456
 //changes made in branch1
 public class Practice {
-	//changes made in master
+	//changes made in master  
 	public static void main(String[] args) {
 		System.out.println("Practice method");
 	}
